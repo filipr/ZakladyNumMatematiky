67a61fb0215a5b50014b1318d670acf2b3a4d815
filipr <roskovec@gmail.com>
2015-10-20 08:45:10 +0200

    ridkaMat

--- 3cvic/ridkaMat.m
+++ 3cvic/ridkaMat.m
@@ -1,3 +1,4 @@
+function ridkaMat
 % porovnani zaplneni QR rozkladu (pomoci mG-S) a LU rozkladu (tedy maticoveho zapisu Gaussovy eliminace)
 
 load('west0479.mat')
@@ -45,3 +46,26 @@ figure(5);
 spy(R), title('Horni trojuhelnikova matice z QR rozkladu')
 nz = nnz(R);
 xlabel(sprintf('nonzeros=%d (%.3f%%)',nz,nz*pct));
+
+end
+
+% modifikovaný Gram-Schmidt
+function [Q, R] = mGS(A)
+m = size(A,2); % pocet sloupcù matice A
+
+Q = zeros(size(A));
+R = zeros(size(A,2));
+
+R(1,1) = norm(A(:,1));
+Q(:,1) = A(:,1)/R(1,1);
+
+for k = 2:m
+    temp = A(:,k); % k-tý sloupec matice A, který bude ortogonalizován
+    for j = 1:k-1 % v MGS se tomuto for-cyklu nelze, narozdíl od cCG, vyhnout
+        R(j,k) = Q(:,j)'*temp;
+        temp = temp - R(j,k)*Q(:,j);
+    end
+    R(k,k) = norm(temp);
+    Q(:,k) = temp/R(k,k);
+end
+end
